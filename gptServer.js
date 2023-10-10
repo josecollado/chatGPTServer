fa655607197d84c5d2625c5856c93ccf3e41f9fb
Jose Collado <jcoll1020@gmail.com>
2023-10-10 11:31:03 -0400

    changed serverjs name to gptServer, so i can see the difference in pm2 list, made sure to bind the server to 0.0.0.0 instead of local host to test and make sure it can be read by all ips

--- gptServer.js
+++ gptServer.js
@@ -31,6 +31,7 @@ app.post('/ask', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
+
