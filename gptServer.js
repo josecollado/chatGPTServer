628f8807e2e61b4fb4caf60ae2423d182c643691
Jose Collado <jcoll1020@gmail.com>
2023-10-10 15:45:29 -0400

    forgot to add the @here method to give me alerts

--- gptServer.js
+++ gptServer.js
@@ -30,12 +30,12 @@ app.post('/ask', async (req, res) => {
     res.json(chatCompletion.choices[0].message.content);
     console.log('sending response to user')
     await axios.post(DISCORD_WEBHOOK_URL, {
-      content: '<><><><><><><><>MESSAGE SENT<><><><><><><><><><>'
+      content: '<><><><>MESSAGE SENT @here<><><><>'
     });
   } catch (error) {
     res.status(500).json({ error: error.message });
     await axios.post(DISCORD_WEBHOOK_URL, {
-      content: `<><><><><><><>MESSAGE ERROR<><><><><><><><
+      content: `<><><><>MESSAGE ERROR @here<><><><>
             ERROR: ${error}`
     });
   }
@@ -43,7 +43,7 @@ app.post('/ask', async (req, res) => {
 
 app.listen(PORT, '0.0.0.0', async () => {
   await axios.post(DISCORD_WEBHOOK_URL, {
-    content: '<><><><><><><><><>GPT SERVER RUNNING<><><><><><><><><>'
+    content: '<><><><><>GPT SERVER RUNNING @here<><><><><>'
   });
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
