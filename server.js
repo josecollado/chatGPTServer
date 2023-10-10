389273ad223d84ea9202ffd2f8a7d6fff0d637ca
Jose Collado <jcoll1020@gmail.com>
2023-10-10 10:01:47 -0400

    ready for migration to aws server

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const OpenAI = require('openai');
+const cors = require('cors');
+
+
 
 dotenv.config();
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
@@ -9,20 +12,20 @@ const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.post('/ask', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
     const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: prompt}],
-      model: 'curie',
-      max_tokens: 5
+      messages: [{ role: "system", content: req.body.prompt }],
+      model: "gpt-3.5-turbo",
+      max_tokens: 80
     });
-    res.json(chatCompletion);
-    console.log(chatCompletion);
+    res.json(chatCompletion.choices[0].message.content);
+    console.log('sending response')
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
