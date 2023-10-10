259c564fb0afb0265053ffa286e4ea0b827a810b
Jose Collado <jcoll1020@gmail.com>
2023-10-10 16:06:29 -0400

    chat gpt refractored

--- gptServer.js
+++ gptServer.js
@@ -1,44 +1,58 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const OpenAI = require('openai');
 const cors = require('cors');
 const axios = require('axios');
 
-
-
-
 dotenv.config();
-const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL
+
+const DISCORD_WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+if (!DISCORD_WEBHOOK_URL || !OPENAI_API_KEY) {
+    console.error("Essential environment variables are missing!");
+    process.exit(1);
+}
+
 const openai = new OpenAI({
   apiKey: OPENAI_API_KEY,
 });
+
 const app = express();
 const PORT = 3005;
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.post('/ask', async (req, res) => {
+  console.log('Received request:', req.body);
+  
   try {
     const chatCompletion = await openai.chat.completions.create({
       messages: [{ role: "system", content: req.body.prompt }],
       model: "gpt-3.5-turbo",
       max_tokens: 80
     });
-    res.json(chatCompletion.choices[0].message.content);
-    console.log('sending response to user')
+    
+    const responseContent = chatCompletion.choices[0].message.content;
+    console.log('sending response to user');
+    
+    res.json(responseContent);
     await axios.post(DISCORD_WEBHOOK_URL, {
       content: '<><><><>MESSAGE SENT @here<><><><>'
     });
   } catch (error) {
+    console.error('Error:', error);
     res.status(500).json({ error: error.message });
+    
     await axios.post(DISCORD_WEBHOOK_URL, {
-      content: `<><><><>MESSAGE ERROR @here<><><><>
-            ERROR: ${error}`
+      content: `<><><><>MESSAGE ERROR @here<><><><>\nERROR: ${error}`
     });
   }
+});
+
+app.get('/health', (req, res) => {
+    res.json({ status: "Server is running" });
 });
 
 app.listen(PORT, '0.0.0.0', async () => {
@@ -47,4 +61,3 @@ app.listen(PORT, '0.0.0.0', async () => {
   });
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
-
